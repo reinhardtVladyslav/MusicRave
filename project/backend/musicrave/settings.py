3b74ad5c0f09572e5e547d81ad101b7de9f9dba3
Nazar Pyskor <nazarii.pyskor@lnu.edu.ua>
2024-11-03 23:27:22 +0200

    Connected AWS RDS PostgreSQL With Django

--- project/backend/musicrave/settings.py
+++ project/backend/musicrave/settings.py
@@ -89,24 +89,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "musicrave.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASE = "simpledbmamager"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "NAME": "musicrave",
+        "USER": "musicrave_admin",
+        "PASSWORD": "musicrave_pass",
+        "HOST": "musicrave-db.craozyflxutr.eu-north-1.rds.amazonaws.com",
         "PORT": "5432",
     }
 }
@@ -154,7 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
