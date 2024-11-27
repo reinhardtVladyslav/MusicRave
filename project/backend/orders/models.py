038662e4ac3492e74d7f58e5fb1df06241876bea
Nazar Pyskor <nazarii.pyskor@lnu.edu.ua>
2024-11-27 18:13:04 +0200

    Created orders app

---
+++ project/backend/orders/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from users.models import Users
+from tracks.models import Track
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
